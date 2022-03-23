65f8b5598d7bca28f330b287d963e039ba8c72a4
frederic rodrigues <fredericlsr@gmail.com>
2022-03-23 14:21:23 -0300

    controle de velocidade

---
+++ script.js
@@ -0,0 +1,13 @@
+
+console.log("================================================================")
+console.log("Radar - Controle de Velocidade")
+console.log("================================================================")
+
+let veloc = 100
+
+if(veloc <= 80){
+    console.log("vc esta dentro do limite permitido, parabens")
+} else{
+    console.log("vc foi multado, velocidade acima do permitido")
+}
+
